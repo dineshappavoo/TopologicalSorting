ecd23293b83ffc2eea43624a275b9c99b852ad90
Dinesh Appavoo <dinesha.cit@gmail.com>
2014-05-07 02:39:44 -0500

    Topological Sorting graph implementation in java

---
+++ src/topologicalsorting/Edge.java
@@ -0,0 +1,25 @@
+package topologicalsorting;
+/**
+ * 
+ */
+
+
+/**
+ * @author Dany
+ *
+ */
+public class Edge {
+
+	
+	public int u;
+	public int v;
+	public int w;
+	
+	public Edge(int u, int v, int w)
+	{
+		this.u=u;
+		this.v=v;
+		this.w=w;
+	}
+	
+}
